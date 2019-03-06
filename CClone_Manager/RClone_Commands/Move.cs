b8ce3bdad76b20ef7d27f701c65c901e3a547957
Jesse Hemphill <jhemphill@mobmed.com>
2019-03-06 17:06:47 -0500

    fixed no internet delay

--- CClone_Manager/RClone_Commands/Move.cs
+++ CClone_Manager/RClone_Commands/Move.cs
@@ -9,48 +9,41 @@ using RClone_Manager.Utilities;
 
 namespace RClone_Manager.RClone_Commands
 {
-    public static class Move 
+    public static class Move
     {
 
-        public static async void moveFile(String rCloneDirectory, String sourceDirectory, String driveTargetDirectory, String fileExtenstion, int maxAttempts)
+        public static  void moveFile(String rCloneDirectory, String sourceDirectory, String driveTargetDirectory, String fileExtenstion, int maxAttempts)
         {
-            int moveAttempts = 0;
-
-
+            int attempts = 0;
             DirectoryInfo d = new DirectoryInfo(sourceDirectory);
             FileInfo[] files = d.GetFiles(String.Format("*.{0}", fileExtenstion)); //Getting Text files
 
-            if (Status.CheckForInternetConnection())
+
+            foreach (FileInfo file in files)
             {
-                if (moveAttempts != 0) { await Task.Delay(3000); }
-
-                while (files.Any() && moveAttempts != maxAttempts)
+                while (attempts != maxAttempts)
                 {
-
-   
-                    foreach (FileInfo file in files)
+                    if (Status.CheckForInternetConnection())
                     {
                         Shell.runRCloneShell("move", rCloneDirectory, file.FullName, driveTargetDirectory);
-
+                        break;
+                    }
+                    else
+                    {
+                        attempts += 1;
+                        System.Threading.Thread.Sleep(300000);
                     }
 
-                 
-                   
-                    moveAttempts += 1;
-                    files = d.GetFiles(fileExtenstion);
-
-                }
-                
                 }
 
             }
 
-
-
-
         }
 
-
-
     }
+}
+
+
+
+
 
