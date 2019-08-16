ffe0fb8ef5a06704341a06523cae9bfaaee25be5
Jesse Hemphill <jhemphill@mobmed.com>
2019-08-16 17:01:50 -0400

    Added exception handling in main

--- File_Archiver/Program.cs
+++ File_Archiver/Program.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using File_Archiver.Processing;
+using NLog;
 
 namespace File_Archiver
 {
     class Program
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
-            Archive.archiveFolder("", "", "", "", "", "", "", "", "");
+            try
+            {
+
+           
+
+            Archive.archiveFolder(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[0], args[0]);
+
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Logger.Error(e, String.Format("{0} : {1}", "Error while assigning archiver parameters", e.Message));
+            }
+
         }
     }
 }
